e2cb29bc94bb8769d7066d08ef3cd3f394c140af
alexkocharyan01 <alexkocharyan97@gmail.com>
2022-10-05 14:56:44 +0400

    navbar responsive change

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -8,7 +8,7 @@ import "./Category.scss";
 
 function Categories() {
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [click, setClick] = useState(false);
 
   const categoriesData = useSelector(state => state.category.data);
 
@@ -23,8 +23,12 @@ function Categories() {
     }
     dispatch(CatImageAsync(value))
     setLoading(true);
-    setIsOpen(false);
+    setClick(false);
   }
+
+  const handleClick = () => setClick(!click);
+
+  const Close = () => setClick(false);
 
   useEffect(() => {
     dispatch(CategoryAsync())
@@ -36,25 +40,32 @@ function Categories() {
     }, 1000);
   }, [loading])
 
-  console.log(isOpen)
-
   return (
-    <div className={`${isOpen ? "main-container" : "navbar_container active"}`}>
-      <div className="menu_icon" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <div className='close'><p>X</p></div> : <div className='open'><svg xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 30 30" width="30px" height="30px"><path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"/></svg></div>}
-      </div>
-      <nav className={isOpen ? "nav_menu active" : "nav_menu"}>
-        <ul>
-          {categoriesData && categoriesData.map((category) => (
-            <li key={category.id} className="nav_ink_items">
-              <NavLink to={`categories/${category.id}`} className="nav_link">
-                <p
+    <div>
+      <div className={click ? 'main-container' : ""} onClick={() => Close()} />
+      <nav className='navbar' onClick={e => e.stopPropagation()}>
+        <div className='nav-container'>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            {categoriesData && categoriesData.map((category) => (
+              <li key={category.id} className="nav-item">
+                <NavLink
+                  to={`categories/${category.id}`}
+                  activeClassName="active"
+                  className="nav-links">
+                  <p
                   onClick={() => getCats(category.id)}
-                >{category.name}</p>
-              </NavLink>
-            </li>
-          ))}
-        </ul>
+                  >{category.name}</p>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+          </div> */}
+          <div className="nav-icon" onClick={handleClick}>
+              {click ? <div className='close'><p>X</p></div> : <div className='open'><svg xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 30 30" width="30px" height="30px"><path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"/></svg></div>}
+          </div>
       </nav>
       {loading && <Loading />}
     </div>
